55540ccb35e7f9a54b3f927484f19a83fa046efc
Yusuf Bilgic <bilgicyusuf@msn.com>
2026-05-25 17:22:32 +0300

    Add black&yellow logo centered at top of page

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ if platform.system() == "Linux":
 
 st.set_page_config(page_title="ASYA Sözleşme Oluşturucu", page_icon="📄", layout="centered")
 
+col1, col2, col3 = st.columns([1, 3, 1])
+with col2:
+    st.image("assets/black&yellow.png", use_container_width=True)
+
 st.title("📄 ASYA OGV Sözleşme Oluşturucu")
 st.markdown("Formu doldurarak EP Sözleşmesi PDF'ini otomatik oluşturun. Asya için sevgiyle tasarlandı.")
 
